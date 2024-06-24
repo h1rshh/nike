cc7ba69c8f61cbdf6a5d08002e86e09ad1cf1fab
h1rshh <harshvardhanshekhawat815gmail.com>
2024-06-24 18:22:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hero from './component/Hero';
+import Navigation from "./component/navigation";
+
+const App = () => {
+    
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Hero></Hero>
+        </div>
+    );
+};
+export default App;
---
+++ src/component/Hero.jsx
@@ -0,0 +1,20 @@
+const Hero = () =>{
+    return <main className="hero">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ipsa, vitae, vel inventore reprehenderit saepe facilis magnam corrupti veritatis eaque quibusdam sed aspernatur perferendis. Alias quas sit distinctio cum fugiat.</p>
+
+            <div className="hero-btn">
+                <button>SHOP NOW</button>
+                <button>CATEGORY</button>
+            </div>
+        </div>
+
+
+        <div className="hero-img">
+            <img src="https://cf.shopee.tw/file/f92affca950131fbe801f6b256d6f607" alt="" />
+        </div>
+    </main>
+};
+
+export default Hero;
